cca4446a4dd3ecc7bd639c3c8d65364e0c1f7b7f
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-10-19 20:23:51 -0500

    Added Shared file for format specifiers and such

---
+++ QRLibrary/Shared.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRLibrary
+{
+    
+    public enum DataStreamEncodingMode { Numeric=0, AlphaNumeric, Byte, Kanji, StructuredAppend, ExtendedChannel, FNC1_1, FNC1_2, EOM }
+
+}
