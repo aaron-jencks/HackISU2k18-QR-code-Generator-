aa46349721c8a459d8b8948b6bd17cf772253d3d
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-10-20 00:12:09 -0500

    Created FNC1 default structure

---
+++ QRLibrary/DataStreamStructure/AQRDataStreamFNC1Payload.cs
@@ -0,0 +1,29 @@
+﻿using QRLibrary.DataStreamStructure.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRLibrary.DataStreamStructure
+{
+    /// <summary>
+    /// A default structure for FNC data streams in the first position
+    /// uses no additional fields
+    /// </summary>
+    public class AQRDataStreamFNC1Payload : AQRDataStream
+    {
+
+        #region constructors
+
+        public AQRDataStreamFNC1Payload(IQRDataStreamEncodingFormat format, IEnumerable<IQRDataStreamData> payload = null) : base(format, payload)
+        {
+        }
+
+        public AQRDataStreamFNC1Payload(string format, IEnumerable<IQRDataStreamData> payload = null) : base(format, payload)
+        {
+        }
+
+        #endregion
+    }
+}
