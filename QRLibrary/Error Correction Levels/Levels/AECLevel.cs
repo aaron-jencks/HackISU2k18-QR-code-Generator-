472260fd696b9650e6e87976b8590be452165851
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-10-20 07:09:59 -0500

    Made a class for the Error Correction levels
    Helps with getting the boolean data for the image

---
+++ QRLibrary/Error Correction Levels/Levels/AECLevel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRLibrary.Error_Correction_Levels.Levels
+{
+    public class AECLevel : IECLevel
+    {
+        public ErrorCorrectionLevel mode { get; set; }
+
+        public AECLevel(ErrorCorrectionLevel level)
+        {
+            mode = level;
+        }
+
+        public bool[] getBooleanData()
+        {
+            bool[] result = new bool[3];
+
+            switch(mode)
+            {
+                case ErrorCorrectionLevel.High:
+                    return new bool[] { false, false };
+                case ErrorCorrectionLevel.Low:
+                    return new bool[] { true, true };
+                case ErrorCorrectionLevel.Medium:
+                    return new bool[] { true, false };
+                case ErrorCorrectionLevel.Q:
+                    return new bool[] { false, true };
+                default:
+                    throw new Exception("Unrecognized Errror Correction Level mode!");
+            }
+        }
+    }
+}
