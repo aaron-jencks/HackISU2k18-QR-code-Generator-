3e21cae2667167b3c207669f193957f07a7cc4de
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-10-20 00:22:03 -0500

    Created default structure for End Of Message payload

---
+++ QRLibrary/DataStreamStructure/AQRDataStreamEOMPayload.cs
@@ -0,0 +1,28 @@
+﻿using QRLibrary.DataStreamStructure.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRLibrary.DataStreamStructure
+{
+    /// <summary>
+    /// A default structure class for an End of Message (EOM) payload
+    /// It contains no data aside from the format specifier
+    /// </summary>
+    public class AQRDataStreamEOMPayload : AQRDataStream
+    {
+        #region constructors
+
+        public AQRDataStreamEOMPayload(IQRDataStreamEncodingFormat format, IEnumerable<IQRDataStreamData> payload = null) : base(format, payload)
+        {
+        }
+
+        public AQRDataStreamEOMPayload(string format, IEnumerable<IQRDataStreamData> payload = null) : base(format, payload)
+        {
+        }
+
+        #endregion
+    }
+}
