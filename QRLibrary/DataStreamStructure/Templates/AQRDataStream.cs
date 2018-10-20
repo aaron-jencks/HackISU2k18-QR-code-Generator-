066c811f0350888d561b381380f72b4a0f149f1c
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-10-19 21:52:45 -0500

    Created an inheritable data stream object class

---
+++ QRLibrary/DataStreamStructure/Templates/AQRDataStream.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QRLibrary.DataStreamFormatSpecifiers.Templates;
+
+namespace QRLibrary.DataStreamStructure.Templates
+{
+    public class AQRDataStream : IQRDataStream
+    {
+        #region properties
+
+        /// <summary>
+        /// The Encoding format for the data stream
+        /// </summary>
+        protected IQRDataStreamEncodingFormat format { get; set; }
+
+        /// <summary>
+        /// The payload of the data stream can have any number, depends on the format
+        /// </summary>
+        protected List<IQRDataStreamData> payload { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public AQRDataStream(IQRDataStreamEncodingFormat format, IEnumerable<IQRDataStreamData> payload = null)
+        {
+            this.format = format;
+            this.payload = payload.ToList() ?? new List<IQRDataStreamData>();
+        }
+
+        public AQRDataStream(string format, IEnumerable<IQRDataStreamData> payload = null)
+        {
+            this.format = new AQRDataStreamEncodingFormat(format);
+            this.payload = payload.ToList() ?? new List<IQRDataStreamData>();
+        }
+
+        #endregion
+
+        #region Inteface methods
+
+        public bool[] getAllData()
+        {
+            List<bool> result = new List<bool>();
+
+            result.AddRange(format.getDataArray());
+
+            foreach (IQRDataStreamData d in payload)
+                result.AddRange(d.getData());
+
+            return result.ToArray();
+        }
+
+        public bool[] getFormatCode()
+        {
+            return format.getDataArray();
+        }
+
+        public bool[] getPayloadCode()
+        {
+            List<bool> result = new List<bool>();
+
+            foreach (IQRDataStreamData d in payload)
+                result.AddRange(d.getData());
+
+            return result.ToArray();
+        }
+
+        #endregion
+    }
+}
