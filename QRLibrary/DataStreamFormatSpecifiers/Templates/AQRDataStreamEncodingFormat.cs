26e0197ac2bc7dc7497fa677879bdeea36a77978
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-10-19 20:48:51 -0500

    Implemented a data stream format interface and class
    Will be used to determine the encoding format of data streams in the code.

---
+++ QRLibrary/DataStreamFormatSpecifiers/Templates/AQRDataStreamEncodingFormat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRLibrary.DataStreamFormatSpecifiers.Templates
+{
+    public abstract class AQRDataStreamEncodingFormat : IQRDataStreamEncodingFormat
+    {
+        /// <summary>
+        /// The boolean data representing the binary code
+        /// </summary>
+        protected bool[] dataArray { get; set; }
+
+        /// <summary>
+        /// Creates a new data stream format code from the given array
+        /// </summary>
+        /// <param name="data">The new data format code</param>
+        public AQRDataStreamEncodingFormat(bool[] data)
+        {
+            dataArray = data;
+        }
+
+        /// <summary>
+        /// Creates a new Data Stream Format code from the given string.
+        /// </summary>
+        /// <param name="data">String specifying the data in the format "0100010100101..."</param>
+        public AQRDataStreamEncodingFormat(string data)
+        {
+            data = data.Trim();
+            dataArray = new bool[data.Length];
+            for(int i = 0; i < data.Length; i++)
+            {
+                switch(data[i])
+                {
+                    case '0':
+                        dataArray[i] = false;
+                        break;
+                    case '1':
+                        dataArray[i] = true;
+                        break;
+                    default:
+                        throw new Exception("Character not recognized in data string");
+                }
+            }
+
+        }
+
+        public bool[] getDataArray()
+        {
+            return dataArray;
+        }
+    }
+}
---
+++ QRLibrary/DataStreamFormatSpecifiers/Templates/IQRDataStreamEncodingFormat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRLibrary
+{
+    public interface IQRDataStreamEncodingFormat
+    {
+        /// <summary>
+        /// Gets the data Array of the object in the form of a boolean array
+        /// </summary>
+        /// <returns>Returns a boolean array that represents the data encoding format</returns>
+        bool[] getDataArray();
+    }
+}
