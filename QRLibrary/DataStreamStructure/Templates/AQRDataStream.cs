ed5d2a8419a4904df73f4cedec7e08db9010991d
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-10-19 21:29:16 -0500

    Made inheritable class for DataStream data object

---
+++ QRLibrary/DataStreamStructure/Templates/AQRDataStream.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRLibrary.DataStreamStructure.Templates
+{
+    public class AQRDataStream : IQRDataStreamData
+    {
+        #region Properties
+
+        /// <summary>
+        /// String identifier representing how to interpret this data
+        /// </summary>
+        protected string identifier { get; set; }
+
+        /// <summary>
+        /// Boolean data stored in this object
+        /// </summary>
+        protected bool[] data { get; set; }
+
+        /// <summary>
+        /// Number of bits in this data block
+        /// </summary>
+        protected int bitCount { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public AQRDataStream(string id, bool[] data)
+        {
+            this.data = data;
+            identifier = id;
+            bitCount = data.Length;
+        }
+
+        public AQRDataStream(string id, string data)
+        {
+            data.Trim();
+            this.data = new bool[data.Length];
+            for(int i = 0; i < data.Length; i++)
+            {
+                switch(data[i])
+                {
+                    case '0':
+                        this.data[i] = false;
+                        break;
+                    case '1':
+                        this.data[i] = true;
+                        break;
+                    default:
+                        throw new Exception("Unrecognized character in data string!");
+                }
+            }
+
+            identifier = id;
+            bitCount = this.data.Length;
+        }
+
+        #endregion
+
+        #region Interface Methods
+
+        public int getBitCount()
+        {
+            return bitCount;
+        }
+
+        public bool[] getData()
+        {
+            return data;
+        }
+
+        public string getIdentifier()
+        {
+            return identifier;
+        }
+
+        #endregion
+    }
+}
---
+++ QRLibrary/DataStreamStructure/Templates/IQRDataStreamData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRLibrary.DataStreamStructure.Templates
+{
+    /// <summary>
+    /// Inteface for creating customized data objects for data streams
+    /// </summary>
+    public interface IQRDataStreamData
+    {
+        /// <summary>
+        /// Returns the data in the data object
+        /// </summary>
+        /// <returns>Returns the data in this object as a boolean array</returns>
+        bool[] getData();
+
+        /// <summary>
+        /// Returns the number of bits allocated to this field
+        /// </summary>
+        /// <returns>Returns a integer stating how many bits are in this field</returns>
+        int getBitCount();
+
+        /// <summary>
+        /// Returns the string identifier for this data object
+        /// </summary>
+        /// <returns>Returns a string identifying how to interpret this object</returns>
+        string getIdentifier();
+    }
+}
